a1c37a353daa966ac19d6df196109440d8bd916b
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-24 08:33:55 -0300

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Rafael Freitas on 24/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Rafael Freitas on 24/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
