1806b3d42f59dc3dbc930cbb23e627cbb6c01e11
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-26 22:07:00 -0300

    Implement intents on viewmodel

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -9,4 +9,25 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     static let palette: String = "🤥😶🤢🤒😈👿🤡💀☠️"
+    
+    @Published var emojiArt: EmojiArt = EmojiArt()
+    
+    //MARK: - Intents
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGFloat){
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.heigth)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int(CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
 }
