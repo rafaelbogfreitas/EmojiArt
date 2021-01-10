bc7e1267bd7d0e79484656106d9207191ca2eac8
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-10 00:46:42 -0300

    Create Palletes extension to manage different palletes

--- EmojiArt/EmojiArtDocumen+Pallete.swift
+++ EmojiArt/EmojiArtDocumen+Pallete.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+extension EmojiArtDocument {
+    private static var PalletesKey = "EmojiArtDocument.PalletesKey"
+    
+    private(set) var palleteNames: [String: String] {
+        get {
+            UserDefaults.standard.object(forKey: EmojiArtDocument.PalletesKey) as? [String: String] ?? [
+                "😀😃😄😁😆😅😂🤣☺️😊😇🙂🙃😉😌😍": "faces",
+                "🍏🍎🍐🍊🍋🍌🍉🍇🍓🍈🍒🍑🥭🍍🥥🥝🍅🍆🥑🥦🥬🥒🌶🌽": "food",
+                "⌚️📱📲💻⌨️🖥🖨🖱🖲🕹🗜💽💾💿📀📼📷📸📹🎥": "objects"
+                
+            ]
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: EmojiArtDocument.PalletesKey)
+            objectWillChange.send()
+        }
+    }
+    
+    var sortedPalletes: [String] {
+        palleteNames.keys.sorted(by: { palleteNames[$0]! < palleteNames[$1]! })
+    }
+    
+    var defaultPalletes: String {
+        sortedPalletes.first ?? "⚠️"
+    }
+    
+    func renamePallete(_ pallete: String, to name: String) {
+        palleteNames[pallete] = name
+    }
+    
+    func addPallete(_ pallete: String, named name: String) {
+        palleteNames[name] = pallete
+    }
+    
+    func removePallete(named name: String) {
+        palleteNames[name] = nil
+    }
+}
