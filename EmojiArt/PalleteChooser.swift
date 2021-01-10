dd535643863992037839101518cabac2291a35e3
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-10 00:47:38 -0300

    Create a stepper to navigate between different palletes

--- EmojiArt/PalleteChooser.swift
+++ EmojiArt/PalleteChooser.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct PalleteChooser: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Stepper(
+                onIncrement: {  },
+                onDecrement: {  },
+                label: {
+                    EmptyView()
+                })
+            
+            Text("Pallete name")
+        }
+        .fixedSize(horizontal: true, vertical: false)
     }
 }
 
