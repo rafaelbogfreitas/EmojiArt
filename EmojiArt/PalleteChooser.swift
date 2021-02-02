e581fb22fad6fb83ee860597fbd7813b5048e795
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-02-01 23:19:13 -0300

    Create remove emojis functionality on pallete

--- EmojiArt/PalleteChooser.swift
+++ EmojiArt/PalleteChooser.swift
@@ -11,6 +11,7 @@ struct PalleteChooser: View {
     
     @ObservedObject var document: EmojiArtDocument
     @Binding var choosenPallete: String
+    @State private var showPalleteEditor: Bool = false
     
     var body: some View {
         HStack {
@@ -24,12 +25,68 @@ struct PalleteChooser: View {
                 label: {
                     EmptyView()
                 })
-            
             Text(self.document.paletteNames[self.choosenPallete] ?? "")
+            Image(systemName: "keyboard").imageScale(.large)
+                .onTapGesture {
+                    showPalleteEditor = true
+                }
+                .popover(isPresented: $showPalleteEditor) {
+                    PalleteEditor(choosenPallete: $choosenPallete)
+                        .environmentObject(document)
+                        .frame(minWidth: 300, minHeight: 500)
+                }
         }
         .fixedSize(horizontal: true, vertical: false)
     }
 }
+
+
+struct PalleteEditor: View {
+    @Binding var choosenPallete: String
+    @EnvironmentObject var document: EmojiArtDocument
+    @State var palleteName: String = ""
+    @State var emojisToAdd: String = ""
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text("PalleteEditor").font(.title).padding()
+            Divider()
+            Form {
+                Section(header: Text("Pallete name")) {
+                    TextField("Pallete name", text: $palleteName, onEditingChanged: { began in
+                        if !began {
+                            document.renamePalette(choosenPallete, to: palleteName)
+                        }
+                    })
+                    TextField("Add emoji", text: $emojisToAdd, onEditingChanged: { began in
+                        if !began {
+                            choosenPallete = document.addEmoji(emojisToAdd, toPalette: choosenPallete)
+                            emojisToAdd = ""
+                        }
+                    })
+                }
+                
+                Section(header: Text("Remove Emoji")) {
+                    VStack {
+                        ForEach(emojisToAdd.map { String($0) }, id: \.self) { emoji in
+                            Text(emoji)
+                                .onTapGesture {
+                                    choosenPallete = document.removeEmoji(emoji, fromPalette: choosenPallete)
+                                }
+                        }
+                    }
+                }
+                
+            }
+            Spacer()
+        }
+        .onAppear {
+            palleteName = document.paletteNames[choosenPallete] ?? ""
+        }
+    }
+}
+
+
 
 struct PalleteChooser_Previews: PreviewProvider {
     static var previews: some View {
