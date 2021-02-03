994124fcf312317de49fe83432206159fab70848
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-02-02 22:24:34 -0300

    Create file for a document chooser view

---
+++ EmojiArt/EmojiArtDocumentChooser.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtDocumentChooser.swift
+//  EmojiArt
+//
+//  Created by Rafael Freitas on 02/02/21.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiArtDocumentChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentChooser()
+    }
+}
