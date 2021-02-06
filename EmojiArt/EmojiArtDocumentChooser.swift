5b148ae6dbf58f34c14684744a2539518b500571
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-02-06 11:44:55 -0300

    Implement document chooser

--- EmojiArt/EmojiArtDocumentChooser.swift
+++ EmojiArt/EmojiArtDocumentChooser.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct EmojiArtDocumentChooser: View {
+    @EnvironmentObject var store: EmojiArtDocumentStore
+    @State private var editMode: EditMode = .inactive
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(store.documents) { document in
+                    NavigationLink(
+                        destination: EmojiArtDocumentView(document: document)
+                            .navigationBarTitle(self.store.name(for: document))
+                    ) {
+                        EditableText(store.name(for: document), isEditing: editMode.isEditing) {
+                            store.setName($0, for: document)
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    indexSet.map { self.store.documents[$0] }.forEach { document in
+                        self.store.removeDocument(document)
+                    }
+                }
+            }
+            .navigationBarTitle(self.store.name)
+            .navigationBarItems(
+                    leading: Button(action: {
+                    self.store.addDocument()
+                }, label: {
+                    Image(systemName: "plus")
+                        .imageScale(.large)
+                }),
+                    trailing: EditButton()
+            )
+            .environment(\.editMode, $editMode)
+        }
     }
 }
 
